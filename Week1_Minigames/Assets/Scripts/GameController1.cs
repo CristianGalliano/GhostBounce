eb4344f679de204131b8165b9bde86d33c36d9f9
Cristian Galliano <34375190+CristianGalliano@users.noreply.github.com>
2019-01-30 22:46:22 +0000

    added player animations and game end conditions
    game will auto end after 10 seconds or if all players die, in future will add if one player survives at 10 seconds they win and get more points.

---
+++ Week1_Minigames/Assets/Scripts/GameController1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController1 : MonoBehaviour
+{
+    private Text timerText;
+    public int count;
+    public GameObject endScreen;
+    public int playerCount;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        timerText = GameObject.Find("Timer").GetComponent<Text>();
+        endScreen.gameObject.SetActive(false);
+        playerCount = PlayerPrefs.GetInt("Players", 0);
+    }
+
+    void Start()
+    {
+        StartCoroutine("timer", count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        allDead();
+    }
+
+    private IEnumerator timer(int sR)
+    {       
+        for (int i = 0; i < count+1; i++)
+        {
+            timerText.text = sR + " Seconds";
+            sR--;
+            yield return new WaitForSeconds(1.0f);
+        }
+        timerText.text = "";
+        endScreen.gameObject.SetActive(true);
+    }
+
+    private void allDead()
+    {
+        if (playerCount == 0)
+        {
+            StopAllCoroutines();
+            timerText.text = "";
+            endScreen.gameObject.SetActive(true);
+        }
+    }
+}
--- Week1_Minigames/Assets/Scripts/PlayerController.cs
+++ Week1_Minigames/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     private TextMesh playerText;
     private GameObject player;
-    private SpriteRenderer playerSprite;
     private Vector3 newPlayerPosition;
-    public Sprite[] sprites;
     public int playerNumber;
+    private Rigidbody2D playerRB;
+    private bool grounded = false;
+    private bool collided = false;
+    private GameController1 gamecontroller1;
+    public bool isdead = false;
+    private Animator playerAnimator;
 
     private void Awake()
     {
         playerText = transform.Find("PlayerText").GetComponent<TextMesh>();
-        playerSprite = transform.Find("PlayerSprite").GetComponent<SpriteRenderer>();
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        gamecontroller1 = GameObject.Find("GameController").GetComponent<GameController1>();
+        playerAnimator = transform.Find("PlayerSprite").GetComponent<Animator>();
     }
 
     // Use this for initialization
@@ -27,94 +33,155 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        playerMovement(playerNumber);   
+        isDead();
+    }
+
+    private void FixedUpdate()
+    {
+        playerMovement(playerNumber);
     }
 
     private void playerMovement(int player)
     {
-        float movement = 0.125f; 
-        switch (player)
+        float movement = 0.125f;
+        float jumpHeight = 5.0f;
+        if (collided == false)
         {
-            case 1:
-                if (Input.GetKey(KeyCode.Z))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[2];
-                    //Debug.Log("z pressed.");
-                }
-                else if (Input.GetKey(KeyCode.X))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[3];
-                    //Debug.Log("x pressed.");
-                }
-                else
-                {
-                    playerSprite.sprite = sprites[0];
-                }
-                break;
-            case 2:
-                if (Input.GetKey(KeyCode.N))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[2];
-                    //Debug.Log("z pressed.");
-                }
-                else if (Input.GetKey(KeyCode.M))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[3];
-                    //Debug.Log("x pressed.");
-                }
-                else
-                {
-                    playerSprite.sprite = sprites[0];
-                }
-                break;
-            case 3:
-                if (Input.GetKey(KeyCode.Alpha1))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[2];
-                    //Debug.Log("z pressed.");
-                }
-                else if (Input.GetKey(KeyCode.Alpha2))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[3];
-                    //Debug.Log("x pressed.");
-                }
-                else
-                {
-                    playerSprite.sprite = sprites[0];
-                }
-                break;
-            case 4:
-                if (Input.GetKey(KeyCode.Alpha8))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[2];
-                    //Debug.Log("z pressed.");
-                }
-                else if (Input.GetKey(KeyCode.Alpha9))
-                {
-                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
-                    transform.position = newPlayerPosition;
-                    playerSprite.sprite = sprites[3];
-                    //Debug.Log("x pressed.");
-                }
-                else
-                {
-                    playerSprite.sprite = sprites[0];
-                }
-                break;
+            switch (player)
+            {
+                case 1:
+                    if (Input.GetKey(KeyCode.Z))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 2);
+                    }
+                    else if (Input.GetKey(KeyCode.X))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 3);
+                    }
+                    else if (Input.GetKey(KeyCode.S) && grounded == true)
+                    {
+                        playerRB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+                    }
+                    else
+                    {
+                        playerAnimator.SetInteger("anim", 1);
+                    }
+                    break;
+                case 2:
+                    if (Input.GetKey(KeyCode.N))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 2);
+                    }
+                    else if (Input.GetKey(KeyCode.M))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 3);
+                    }
+                    else if (Input.GetKey(KeyCode.J) && grounded == true)
+                    {
+                        playerRB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+                    }
+                    else
+                    {
+                        playerAnimator.SetInteger("anim", 1);
+                    }
+                    break;
+                case 3:
+                    if (Input.GetKey(KeyCode.Alpha1))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 2);
+                    }
+                    else if (Input.GetKey(KeyCode.Alpha2))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 3);
+                    }
+                    else if (Input.GetKey(KeyCode.Q) && grounded == true)
+                    {
+                        playerRB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+                    }
+                    else
+                    {
+                        playerAnimator.SetInteger("anim", 1);
+                    }
+                    break;
+                case 4:
+                    if (Input.GetKey(KeyCode.Alpha8))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 2);
+                    }
+                    else if (Input.GetKey(KeyCode.Alpha9))
+                    {
+                        newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                        transform.position = newPlayerPosition;
+                        playerAnimator.SetInteger("anim", 3);
+                    }
+                    else if (Input.GetKey(KeyCode.I) && grounded == true)
+                    {
+                        playerRB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+                    }
+                    else
+                    {
+                        playerAnimator.SetInteger("anim", 1);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            collided = false;
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            collided = true;
+            if (collision.contacts[0].point.x > collision.collider.bounds.center.x)
+            {
+                Vector2 knockbackVelocity = new Vector2(3.5f, 3.5f);
+                playerRB.AddForce(knockbackVelocity,ForceMode2D.Impulse);
+            }
+            else if (collision.contacts[0].point.x < collision.collider.bounds.center.x)
+            {
+                Vector2 knockbackVelocity = new Vector2(-3.5f, 3.5f);
+                playerRB.AddForce(knockbackVelocity,ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    private void isDead()
+    {
+        if (transform.position.y <= -5.0f)
+        {
+            isdead = true;
+        }
+        if (isdead == true)
+        {
+            gamecontroller1.playerCount--;
+            Destroy(gameObject);
         }
     }
 }
