3cb39c18612705533224d64cd394c47991829b67
Cristian Galliano <34375190+CristianGalliano@users.noreply.github.com>
2019-01-30 12:43:50 +0000

    implement basic character controls and spawner

---
+++ Week1_Minigames/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private TextMesh playerText;
+    private GameObject player;
+    private SpriteRenderer playerSprite;
+    private Vector3 newPlayerPosition;
+    public Sprite[] sprites;
+    public int playerNumber;
+
+    private void Awake()
+    {
+        playerText = transform.Find("PlayerText").GetComponent<TextMesh>();
+        playerSprite = transform.Find("PlayerSprite").GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerText.text = playerNumber + "P";
+        //Debug.Log(playerNumber);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerMovement(playerNumber);   
+    }
+
+    private void playerMovement(int player)
+    {
+        float movement = 0.125f; 
+        switch (player)
+        {
+            case 1:
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[2];
+                    //Debug.Log("z pressed.");
+                }
+                else if (Input.GetKey(KeyCode.X))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[3];
+                    //Debug.Log("x pressed.");
+                }
+                else
+                {
+                    playerSprite.sprite = sprites[0];
+                }
+                break;
+            case 2:
+                if (Input.GetKey(KeyCode.N))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[2];
+                    //Debug.Log("z pressed.");
+                }
+                else if (Input.GetKey(KeyCode.M))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[3];
+                    //Debug.Log("x pressed.");
+                }
+                else
+                {
+                    playerSprite.sprite = sprites[0];
+                }
+                break;
+            case 3:
+                if (Input.GetKey(KeyCode.Alpha1))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[2];
+                    //Debug.Log("z pressed.");
+                }
+                else if (Input.GetKey(KeyCode.Alpha2))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[3];
+                    //Debug.Log("x pressed.");
+                }
+                else
+                {
+                    playerSprite.sprite = sprites[0];
+                }
+                break;
+            case 4:
+                if (Input.GetKey(KeyCode.Alpha8))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[2];
+                    //Debug.Log("z pressed.");
+                }
+                else if (Input.GetKey(KeyCode.Alpha9))
+                {
+                    newPlayerPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
+                    transform.position = newPlayerPosition;
+                    playerSprite.sprite = sprites[3];
+                    //Debug.Log("x pressed.");
+                }
+                else
+                {
+                    playerSprite.sprite = sprites[0];
+                }
+                break;
+        }
+    }
+}
---
+++ Week1_Minigames/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject[] players;
+	// Use this for initialization
+	void Start ()
+    {
+        int i = PlayerPrefs.GetInt("Players", 1);
+        spawn(i);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void spawn(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                Instantiate(players[0], new Vector3(0, 2, 0), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(players[0], new Vector3(-4, 2, 0), Quaternion.identity);
+                Instantiate(players[1], new Vector3(4, 2, 0), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(players[0], new Vector3(-4, 2, 0), Quaternion.identity);
+                Instantiate(players[1], new Vector3(0, 2, 0), Quaternion.identity);
+                Instantiate(players[2], new Vector3(4, 2, 0), Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(players[0], new Vector3(-8, 2, 0), Quaternion.identity);
+                Instantiate(players[1], new Vector3(-4, 2, 0), Quaternion.identity);
+                Instantiate(players[2], new Vector3(4, 2, 0), Quaternion.identity);
+                Instantiate(players[3], new Vector3(8, 2, 0), Quaternion.identity);
+                break;
+        }
+    }
+}
